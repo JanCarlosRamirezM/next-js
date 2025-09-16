0332a670890363724f3e5bc20bfd71e15e998d86
Ing. Jan Carlos Ramirez M. <jancarlosramirez2@gmail.com>
2025-09-16 18:06:15 -0400

    feat: initialize Next.js project with TypeScript and Tailwind CSS
    - Added package.json with dependencies for React, Next.js, TypeScript, and Tailwind CSS.
    - Created postcss.config.mjs for Tailwind CSS integration.
    - Added SVG assets for icons and logos in the public directory.
    - Configured TypeScript with strict settings and Next.js support in tsconfig.json.

---
+++ 01.first-steps/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center">
+      <span className="text-5xl">Mi primera app en next-js</span>
+    </div>
+  );
+}
