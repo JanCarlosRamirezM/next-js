104bc1648b7815c1cf7b339450009305e7527102
Ing. Jan Carlos Ramirez M <38397907+JanCarlosRamirezM@users.noreply.github.com>
2025-10-03 14:23:31 -0400

    creacion del proyecto

---
+++ 03.teslo-shop/src/app/page.tsx
@@ -0,0 +1,11 @@
+import { titleFont } from "@/config";
+
+export default function Home() {
+  return (
+    <div className="">
+      <h1>Hola</h1>
+
+      <h1 className={titleFont.className}>Hola 2</h1>
+    </div>
+  );
+}
